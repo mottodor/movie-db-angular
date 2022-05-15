be6227cf10934e95181427213515881d3d6ef838
Kantemir Shogenov <kantemir-83@yandex.ru>
2022-05-15 23:14:10 +0300

    refactor movies service

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -5,11 +5,14 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class MoviesService {
+  baseUrl = 'https://api.themoviedb.org/3';
+  apiKey = '203fc58abdee389497e2331ece97037d';
+
   constructor(private http: HttpClient) {}
 
-  getMovies() {
+  getMovies(type: string = 'popular') {
     return this.http.get(
-      'https://api.themoviedb.org/3/movie/upcoming?api_key=203fc58abdee389497e2331ece97037d'
+      `${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`
     );
   }
 }
